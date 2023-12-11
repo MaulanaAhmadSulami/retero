2bb6062efc567eb634c3e68d1205404f136ab835
kornel <korneliarhyu2@gmail.com>
2023-12-11 23:31:25 +0700

    cobaan

--- resources/views/homepage/searchProduct.blade.php
+++ resources/views/homepage/searchProduct.blade.php
@@ -1,37 +1,77 @@
 @extends('layouts.app')
 @section('title', 'Retero - Search')
 @push('styles')
+<style>
+    #card-shadow {
+        box-shadow: 0.3rem 0.3rem 0;
+        transition: transform 0.2s, box-shadow 0.2s;
+    }
 
+    #button-shadow {
+        box-shadow: 0.2rem 0.2rem 0;
+        transition: transform 0.2s, box-shadow 0.2s;
+    }
+
+    #button-shadow:hover {
+        transform: translate3d(0.25rem, 0.25rem, -0.25rem);
+        box-shadow: none;
+    }
+    
+    #card-effect {
+        text-shadow:
+            -1px -1px 0 #fff,
+            1px -1px 0 #fff,
+            -1px 1px 0 #fff,
+            1px 1px 0 #fff;
+    }
+
+    #stroke {
+        text-shadow:
+            -3px -3px 0 #000,
+            3px -3px 0 #000,
+            -3px 3px 0 #000,
+            3px 3px 0 #000;
+    }
+</style>
 @endpush
 
 @section('content')
-<div class="py-10 bg-[#FAFAFA]">
+<div class="py-4 bg-[#FAFAFA]">
     <div class="container mx-auto px-4">
-        <h1 class="text-center text-[56px] font-ubuntuMonoRegular mb-6">Search results for : "{{ $query }}"</h1>
+        <h1 class="text-center text-[42px] font-ubuntuMonoBold mb-2">Search results for : "{{ $query }}"</h1>
         <div class="flex justify-center items-center">
             <form id="search-form" method="GET" action="{{ route('homepage.searchProduct') }}"
                 class="w-full flex flex-row items-center justify-center">
                 <input type="text" name="query" placeholder="Retero is looking for it...!"
-                    class="pl-4 pr-4 py-8 px-8 shadow-lg rounded-lg w-2/5" />
-                <button type="submit"
-                    class="bg-button font-ubuntuMonoBold text-[#000000] text-[25px] ml-12 rounded-lg border py-6 px-6">
-                    Search
+                    class="pl-4 pr-2 py-6 px-8 rounded-lg w-2/5 bg-[#fefefe] border border-[#c6c6c6] h-6" />
+                <button type="submit" 
+                    id="button-shadow"
+                    class="bg-button font-ubuntuMonoBold text-[#000000] text-[18px] ml-6 rounded-lg border py-2 px-4">
+                    <p class="hover:text-[#FFFFFF]">Search</p>
                 </button>
             </form>
         </div>
     </div>
 </div>
 
-<div class="flex items-center justify-center py-10">
-    <div class="container px-4 py-10">
-        <div id="products-container" class="grid grid-cols-1 md:grid-cols-3 gap-4">
+<div class="flex items-center justify-center py-6 font-ubuntuMonoBold">
+    <div class="container px-4 py-4">
+        <div id="products-container" class=" grid grid-cols-2 md:grid-cols-3 gap-7 w-1/2 mx-auto">
             @forelse ($products as $product)
-            <div id="card-shadow" class="border shadow-md p-4 flex flex-col justify-between">
+            <div id="card-shadow" class="border shadow-md p-4 mb-2 flex flex-col justify-between">
                 <img src="{{ asset('images/' . $product->image) }}" alt="{{ $product->productTitle }}"
                     class="w-full h-32 object-cover">
-                <div class="mt-2 font-bold text-center border-b py-2 mb-4">{{ $product->productTitle }}</div>
-                <a href="{{ route('homepage.productDetail', ['id' => $product->id]) }}"
-                    class="bg-button text-white px-4 py-2 rounded-lg font-ubuntuMonoBold text-center">Baca Ulasan</a>
+                <div class="mt-2 font-bold text-center py-2">
+                    <span class="border-b-2 border-[#FFC800] text-[21px]">
+                        {{ $product->productTitle }}
+                    </span>
+                </div>
+                
+                <button id="button-shadow" type="button" class="bg-button flex self-center my-4  px-2">
+                    <a href="{{ route('homepage.productDetail', ['id' => $product->id]) }}" id="card-effect" class="font-ubuntuMonoBold px-1">Baca
+                        Ulasan</a>
+                </button>
+
             </div>
             @empty
             <div class="col-span-3 flex justify-center items-center h-64">
