2830cfc6ec5b5e61c710380a4b616409ab3af713
Kakaede <maulanasulami271@gmail.com>
2023-12-19 18:17:31 +0700

    more minor styling

--- resources/views/homepage/profile.blade.php
+++ resources/views/homepage/profile.blade.php
@@ -2,8 +2,15 @@
 @section('title', 'Profile - ' . $user->name)
 @push('styles')
 <style>
-   
+    #shadow-btn {
+        box-shadow: 0.5rem 0.5rem 0;
+        transition: transform 0.2s, box-shadow 0.2s;
+    }
 
+    #shadow-btn:hover {
+        transform: translate3d(0.25rem, 0.25rem, -0.25rem);
+        box-shadow: none;
+    }
 </style>
 @endpush
 
@@ -14,7 +21,9 @@
         <div class="container border max-w-6xl">
             <div class="p-6">
                 <div class="flex items-center justify-start">
-                    <img class="w-[15%] mr-5" src="{{ $user->avatar ? asset('storage/' . $user->avatar) : asset('images/dummyStock.png') }}" alt="ProfilePicture" class="w-32 h-32 object-cover rounded-lg mr-4">
+                    <img class="w-[15%] mr-5"
+                        src="{{ $user->avatar ? asset('storage/' . $user->avatar) : asset('images/dummyStock.png') }}"
+                        alt="ProfilePicture" class="w-32 h-32 object-cover rounded-lg mr-4">
                     <div>
                         <h1 class="text-xl font-bold">{{ $user->name }}</h1>
                         <div class="mb-5">
@@ -22,10 +31,12 @@
                             <span data-timestamp="{{ $user->created_at->timestamp }}"></span>
                         </div>
                         @if(Auth::check() && $user->id == Auth::user()->id)
-                        <a href="{{ route('user.edit') }}" class="bg-button py-2 px-2 rounded-lg">
-                            <i class="fa-solid fa-gear"></i>
-                            <span class="">Settings</span>
-                        </a>                            
+                        <button id="shadow-btn" class="bg-button py-2 px-2 rounded-lg">
+                            <a href="{{ route('user.edit') }}">
+                                <i class="fa-solid fa-gear"></i>
+                                <span class="">Settings</span>
+                            </a>
+                        </button>
                         @endif
                     </div>
                 </div>
@@ -40,16 +51,18 @@
             </h3>
             <div class="mt-5">
                 @foreach ($comments as $comment)
-                    <div class="flex items-start gap-4 mb-4">
-                        <img src="{{ $user->avatar ? asset('storage/' . $user->avatar) : asset('images/dummyStock.png') }}" class="rounded-full h-10 w-10 flex-shrink-0" alt="">
-                        <div>
-                            <div class="flex items-center">
-                                <h1 class="font-ubuntuMonoBold">{{ $user->name }}</h1>
-                                <p class="text-sm text-gray-500 ml-2 font-ubuntuMonoBold" data-timestamp="{{ $comment->created_at->timestamp }}"></p>
-                            </div>
-                            <p class="font-ubuntuMonoRegular">{{ $comment->reviewComment }}</p>
+                <div class="flex items-start gap-4 mb-4">
+                    <img src="{{ $user->avatar ? asset('storage/' . $user->avatar) : asset('images/dummyStock.png') }}"
+                        class="rounded-full h-10 w-10 flex-shrink-0" alt="">
+                    <div>
+                        <div class="flex items-center">
+                            <h1 class="font-ubuntuMonoBold">{{ $user->name }}</h1>
+                            <p class="text-sm text-gray-500 ml-2 font-ubuntuMonoBold"
+                                data-timestamp="{{ $comment->created_at->timestamp }}"></p>
                         </div>
+                        <p class="font-ubuntuMonoRegular">{{ $comment->reviewComment }}</p>
                     </div>
+                </div>
                 @endforeach
             </div>
         </div>
--- resources/views/homepage/profileEdit.blade.php
+++ resources/views/homepage/profileEdit.blade.php
@@ -1,68 +1,86 @@
 @extends('layouts.app')
 @section('title', 'Edit User - ' . Auth::user()->name)
 @push('styles')
-    <style>
+<style>
+    #shadow-btn {
+        box-shadow: 0.5rem 0.5rem 0;
+        transition: transform 0.2s, box-shadow 0.2s;
+    }
 
-    </style>
+    #shadow-btn:hover {
+        transform: translate3d(0.25rem, 0.25rem, -0.25rem);
+        box-shadow: none;
+    }
+</style>
 @endpush
 
 @section('content')
 <div class="mx-16">
-<div class="container border rounded-lg mx-auto max-w-full w-full p-4 m-4 text-2xl">
-    <h2 class="text-[25px] font-publicPixel mb-12 mt-4 text-center">Edit Profile</h2>
-    @if (session('message'))
+    <div class="container border rounded-lg mx-auto max-w-full w-full p-4 m-4 text-2xl">
+        <h2 class="text-[25px] font-publicPixel mb-12 mt-4 text-center">Edit Profile</h2>
+        @if (session('message'))
         <div class="bg-[#4ade80] text-white p-4 mb-4 text-center font-publicPixel">
             {{ session('message') }}
         </div>
-    @endif
+        @endif
 
-    <div class="mx-40">
-    <form action="{{ route('user.update') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-10">
-            <label class="block font-ubuntuMonoBold mb-4" for="username">Username</label>
-            <input class="shadow border rounded py-2 px-3 w-full" id="username" type="text" name="name" value="{{ Auth::user()->name }}" required>
+        <div class="mx-40">
+            <form action="{{ route('user.update') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="mb-10">
+                    <label class="block font-ubuntuMonoBold mb-4" for="username">Username</label>
+                    <input class="shadow border rounded py-2 px-3 w-full" id="username" type="text" name="name"
+                        value="{{ Auth::user()->name }}" required>
+                </div>
+
+                <div class="mb-10">
+                    <label class="block font-ubuntuMonoBold mb-4" for="avatar">Avatar</label>
+                    <img class="w-[13%] rounded-full"
+                        src="{{ Auth::user()->avatar ? asset('storage/' . Auth::user()->avatar) : asset('images/dummyStock.png') }}"
+                        alt="Profile Avatar">
+                    <input class="rounded py-2 w-full" id="avatar" type="file" name="avatar"
+                        value="{{ Auth::user()->avatar }}">
+                </div>
+
+                <h3 class="font-ubuntuMonoRegular mb-4">If you want to change your password, enter your old and new
+                    password below!</h3>
+
+                <div class="mb-10">
+                    <label class="block  font-ubuntuMonoBold mb-4" for="password">Old Password</label>
+                    <input class="shadow border rounded py-2 px-3 w-full" placeholder="Old Password" id="old_password"
+                        type="password" name="old_password">
+                    @error('old_password')
+                    <span>{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="mb-10">
+                    <label class="block  font-ubuntuMonoBold mb-4" for="password">New Password</label>
+                    <input class="shadow border rounded py-2 px-3 w-full" placeholder="New Password" id="new_password"
+                        type="password" name="new_password">
+                    @error('new_password')
+                    <span>{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="mb-10">
+                    <label class="block font-ubuntuMonoBold" for="password_confirmation">Confirm</label>
+                    <input class="shadow border rounded py-2 px-3 w-full" id="new_password_confirmation"
+                        placeholder="Confirm new password" type="password" name="password_confirmation">
+                    @error('password_confirmation')
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="flex items-center justify-between mb-10">
+                    <button id="shadow-btn"
+                        class="bg-button text-[#000] py-4 px-4 rounded-lg flex items-center gap-2 mt-2 hover:bg-[#facc15]"
+                        type="submit">
+                        <span class="font-ubuntuMonoBold">Save changes</span>
+                    </button>
+                </div>
+            </form>
         </div>
-
-        <div class="mb-10">
-            <label class="block font-ubuntuMonoBold mb-4" for="avatar">Avatar</label>
-            <img class="w-[13%] rounded-full" src="{{ Auth::user()->avatar ? asset('storage/' . Auth::user()->avatar) : asset('images/dummyStock.png') }}" alt="Profile Avatar">
-            <input class="rounded py-2 w-full" id="avatar" type="file" name="avatar" value="{{ Auth::user()->avatar }}">
-        </div>
-
-        <h3 class="font-ubuntuMonoRegular mb-4">If you want to change your password, enter your old and new password below!</h3>
-
-        <div class="mb-10">
-            <label class="block  font-ubuntuMonoBold mb-4" for="password">Old Password</label>
-            <input class="shadow border rounded py-2 px-3 w-full" placeholder="Old Password" id="old_password" type="password" name="old_password" >
-            @error('old_password')
-                <span>{{ $message }}</span>
-            @enderror
-        </div>
-
-        <div class="mb-10">
-            <label class="block  font-ubuntuMonoBold mb-4" for="password">New Password</label>
-            <input class="shadow border rounded py-2 px-3 w-full" placeholder="New Password" id="new_password" type="password" name="new_password" >
-            @error('new_password')
-                <span>{{ $message }}</span>
-            @enderror
-        </div>
-
-        <div class="mb-10">
-            <label class="block font-ubuntuMonoBold" for="password_confirmation">Confirm</label>
-            <input class="shadow border rounded py-2 px-3 w-full" id="new_password_confirmation" placeholder="Confirm new password" type="password" name="password_confirmation">
-            @error('password_confirmation')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-
-        <div class="flex items-center justify-between mb-10">
-            <button class="bg-button text-[#000] py-4 px-4 rounded-lg flex items-center gap-2 mt-2 hover:bg-[#facc15]" type="submit">
-                <span class="font-ubuntuMonoBold">Save changes</span>
-            </button>
-        </div>
-    </form>
     </div>
-</div>
 </div>
 @endsection
