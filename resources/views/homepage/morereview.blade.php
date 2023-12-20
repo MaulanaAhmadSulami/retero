e12b21fa7966737bd79925357152390a1ae271aa
kornel <korneliarhyu2@gmail.com>
2023-12-21 03:34:53 +0700

    rapihin aja, carousell surren

--- resources/views/homepage/morereview.blade.php
+++ resources/views/homepage/morereview.blade.php
@@ -3,16 +3,11 @@
 @push('styles')
 <style>
     #stroke {
-        text-shadow:
-            -3px -3px 0 #fff,
-            3px -3px 0 #fff,
-            -3px 3px 0 #fff,
-            3px 3px 0 #fff;
-
-        color:rgb(0, 0, 0);
-
-        -webkit-text-stroke-width: 3px;
-
+    text-shadow:
+    -3px -3px 0 #fff,
+    3px -3px 0 #fff,
+    -3px 3px 0 #fff,
+    3px 3px 0 #fff;
     }
 
     #shadow-btn {
@@ -44,61 +39,74 @@
     .swiper-slide {
         width: 500px;
     }
-
-    .mySwiper {
-        display: flex;
-        justify-content: center;
+        
+    .swiper {
+        width: 60rem;
+        height: 23rem;
+        margin: 0 auto;
     }
 
-    
-    
 </style>
 @endpush
 
 @section('content')
 <div>
-    <div id="stroke" class="relative">
-
-        <div class="p-4 bg-button flex justify-center items-center h-32 font-bold font-publicPixel">
-            <h2 class="text-[35px]">
-                Ulasan Lainnya
-            </h2>
-        </div>
-
+    <div id="stroke" class="title p-4 border-b-4 bg-button flex justify-center font-bold font-publicPixel">
+        <h2 class="text-[24px]">
+            Ulasan Lainnya
+        </h2>
     </div>
 </div>
 
-<div class="p-10">
-    <div class="flex flex-col items-center">
-        <div class="w-full max-w-max">
-            @foreach($productsByType as $productType => $products)
-                <h2 class="mb-8 mt-20 flex text-left font-ubuntuMonoBold text-[25px]">{{ $productType }}</h2>
-                <div class="swiper mySwiper">
-                    <div class="swiper-wrapper borer">
-                        @foreach ($products as $product)
-                            <div class="swiper-slide">
-                                <div id=card-shadow class="border  shadow-md 2 p-4 w-[250px] flex flex-col justify-between  ">
-                                    <img src="{{ Storage::disk('public')->exists($product->image) ? asset('storage/' . $product->image) : asset('images/' . $product->image) }}"
-                                        alt="Image Unavailable" class="w-full h-32 object-cover">
-                                    <div class="mt-2 font-ubuntuMonoBold text-center border-b py-2 mb-4">{{
-                                        $product->productTitle }}</div>
-                                    <button id="shadow-btn" type="button" class="bg-button flex self-end">
-                                        <a href="{{ route('homepage.productDetail', ['id' => $product->id]) }}" id="card-effect"
-                                            class="font-ubuntuMonoBold px-1">Baca
-                                            Ulasan</a>
-                                    </button>
-                                </div>
+<div class="flex justify-center">
+    <div class="container overflow-x-auto grid mb-10 mt-4 ">
+        {{-- cards product --}}
+        @foreach($productsByType as $productType => $products)
+
+        {{-- ini aku opacity in 0% karena aku bingung kasih gap vertikal dari grid ke grid --}}
+        <h2 class="text-left w-[63%] mx-auto font-ubuntuMonoBold text-[25px]" style="opacity: 0%">{{ $productType }}</h2>
+
+        <div class="container-section border-t-2 border-[#fbbf24] py-6 w-[65%] mx-auto shadow-md">
+            <div class="swiper mySwiper md:grid-cols-3 mx-auto ">
+                {{-- card product --}}
+                <h2 class="text-left pl-6 pb-2 mx-auto font-ubuntuMonoBold text-[25px]">{{ $productType }}</h2>
+                <div class="swiper-wrapper">
+                    @foreach ($products as $product)
+                    <div class="swiper-slide pb-12 pl-6">
+                        <div id=card-shadow class="border p-4 mb-2 flex flex-col h-full">
+    
+                            {{-- image container --}}
+                            <img src="{{ Storage::disk('public')->exists($product->image) ? asset('storage/' . $product->image) : asset('images/' . $product->image) }}"
+                                alt="Image Unavailable" class="w-full h-32 object-cover">
+                            
+                            {{-- product title --}}
+                            <div class="flex-grow mt-2 font-bold text-center py-2">
+                                <span class="border-b-2 border-[#FFC800] text-[21px]">
+                                    {{ $product->productTitle }}
+                                </span>
                             </div>
-                        @endforeach
+    
+                            <button id="shadow-btn" type="button" class="bg-button flex self-center my-4  px-2">
+                                <a href="{{ route('homepage.productDetail', ['id' => $product->id]) }}" id="card-effect"
+                                    class="font-ubuntuMonoBold px-1">
+                                    Baca Ulasan</a>
+                            </button>
+                        </div>
                     </div>
-                    <div class="mt-10">
-                        <div class="swiper-button-next"></div>
-                        <div class="swiper-button-prev"></div>
-                        <div class="swiper-pagination"></div>
-                    </div>
+                    @endforeach
+    
                 </div>
-            @endforeach
+    
+                {{-- swiper --}}
+                <div class="swipper mt-10">
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
         </div>
+        @endforeach
+
     </div>
 </div>
 
@@ -108,7 +116,7 @@
         swipers.forEach(function(swiper) {
             new Swiper(swiper, {
                 slidesPerView: 4,
-                spaceBetween: 30,
+                spaceBetween: 10,
                 loop: true,
                 pagination: {
                     el: swiper.querySelector('.swiper-pagination'),
